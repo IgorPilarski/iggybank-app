7f1efd37f6da9aaa2e69a9a0e2f908433aed4351
Igor Pilarski <avgorpl@gmail.com>
2025-06-15 17:48:08 +0200

    DB created

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using iggybank_app.Models;
+
+namespace iggybank_app.Data;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+}
