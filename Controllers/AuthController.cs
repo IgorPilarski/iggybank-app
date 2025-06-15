bcddb6f725aab8e85d0a37ab3a6780eebde0bcbc
Igor Pilarski <avgorpl@gmail.com>
2025-06-15 18:10:18 +0200

    Register controller added

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using iggybank_app.Data;
+using iggybank_app.Models;
+
+namespace iggybank_app.Controllers
+{
+    [ApiController]
+    [Route("api/auth")] 
+    public class AuthController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public AuthController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        
+        [HttpPost("register")]
+        public IActionResult Register([FromBody] User request)
+        {
+            if (_context.Users.Any(u => u.Username == request.Username))
+            {
+                return BadRequest("Username already exists");
+            }
+            var user = new User();
+            user.Username = request.Username;
+            user.PasswordHash = request.PasswordHash;
+            _context.Users.Add(user);
+            _context.SaveChanges();
+            return Ok("User has been registered!");
+        }
+    }
+}
